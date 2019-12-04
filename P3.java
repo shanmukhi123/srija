c56b3dede14a70c54bb035c0447be8d9067c68f4
shanmukhi123 <58499682+shanmukhi123@users.noreply.github.com>
2019-12-04 10:03:20 +0530

    Assignment 1
    Commandline argument programs

---
+++ P3.java
@@ -0,0 +1,12 @@
+class P3
+{
+   public static void main(String args[])
+{
+   int a;
+   int b;
+   a=Integer.parseInt(args[0]);
+   b=Integer.parseInt(args[1]);
+int c=a+b;
+System.out.println("Sum of "+ a +" and "+b+" is "+ c);
+}
+}
